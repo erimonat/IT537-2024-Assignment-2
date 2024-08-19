2ed31bf5747dd48435e40b547a7fa72650326894
erimonat <152520084+erimonat@users.noreply.github.com>
2024-08-20 00:38:02 +0300

    7th commit

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,12 +3,12 @@
     <h1 class="text-4xl font-bold mb-4 bg-red-600 text-white p-2 rounded">Pokedex</h1>
     <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
       <div
-        v-for="(pokemon, index) in pokemons"
-        :key="pokemon.name"
-        :class="['shadow-md', 'rounded', 'p-4', 'text-center', bgColor(index)]"
+        v-for="pokemon in pokemons"
+        :key="pokemon.id"
+        :class="['shadow-md', 'rounded', 'p-4', 'text-center', bgColor(pokemon.id)]"
       >
         <router-link :to="{ name: 'PokemonDetails', params: { name: pokemon.name } }">
-          <img :src="getPokemonImageUrl(index + 1 + offset)" :alt="pokemon.name" class="w-24 h-24 object-cover mb-2 mx-auto" />
+          <img :src="getPokemonImageUrl(pokemon.id)" :alt="pokemon.name" class="w-24 h-24 object-cover mb-2 mx-auto" />
           <p class="text-lg font-medium capitalize">{{ pokemon.name }}</p>
         </router-link>
       </div>
@@ -30,7 +30,7 @@ export default {
     return {
       pokemons: [],
       offset: 0,
-      limit: 10,
+      limit: 20,
     };
   },
   mounted() {
@@ -40,7 +40,14 @@ export default {
     async fetchPokemons() {
       try {
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${this.offset}&limit=${this.limit}`);
-        this.pokemons = this.pokemons.concat(response.data.results);
+        const newPokemons = await Promise.all(response.data.results.map(async (pokemon) => {
+          const details = await axios.get(pokemon.url);
+          return {
+            name: details.data.name,
+            id: details.data.id,
+          };
+        }));
+        this.pokemons = [...this.pokemons, ...newPokemons];
         this.offset += this.limit;
       } catch (error) {
         console.error(error);
